c036cbc923d5f788c4692ba27e00cb58b486d314
Tobias Gemperli <Tobias.Gemperli@cleverandson.com>
2014-06-06 14:32:44 +0200

    remove perspective correction

--- instaoverlay/MAImagePickerController/MAImagePickerControllerAdjustViewController.mm
+++ instaoverlay/MAImagePickerController/MAImagePickerControllerAdjustViewController.mm
@@ -46,7 +46,7 @@
     
     _sourceImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height - kCameraToolBarHeight)];
     [_sourceImageView setContentMode:UIViewContentModeScaleAspectFit];
-    
+	
 	
 	NSLog(@"size %f %f",_sourceImage.size.width,_sourceImage.size.height);
 	
@@ -264,45 +264,20 @@
     int const maxImagePixelsAmount = 6200000; // 3.2 MP
 	// UIImage* newImage =scaleAndRotateImage(_adjustedImage, maxImagePixelsAmount);
 
-	UIImage* newImage =scaleAndRotateImage(_adjustedImage, maxImagePixelsAmount);
+	// UIImage* newImage =scaleAndRotateImage(_adjustedImage, maxImagePixelsAmount);
 
-	UIImage* splitImage = [self split:newImage];
-	
-	
-	NSLog(@"size 1 %f %f",_adjustedImage.size.width,splitImage.size.height);
-	NSLog(@"size 2 %f %f",newImage.size.width,newImage.size.height);
+
+
 
 	CRecognitionViewController* recognitionController = [CRecognitionViewController sharedManager];
 	[[self navigationController] pushViewController:recognitionController animated:YES];
 	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, .3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-		[recognitionController recognizeImage:newImage];
+		[recognitionController recognizeImage:_adjustedImage];
 	});
 	
 	[[self navigationController] setNavigationBarHidden:NO animated:NO];
 
-
-	
 }
-
-
-
--(UIImage *)split:(UIImage *)im {
-	CGSize size = [im size];
-	
-	int i = 1;
-	int div = 2;
-	CGRect portion = CGRectMake(i * size.width/div,0, size.width/div, size.height);
-	UIGraphicsBeginImageContext(portion.size);
-	CGContextRef context = UIGraphicsGetCurrentContext();
-	CGContextScaleCTM(context, 1.0, -1.0);
-	CGContextTranslateCTM(context, 0, -portion.size.height);
-	CGContextTranslateCTM(context, -portion.origin.x, -portion.origin.y);
-	CGContextDrawImage(context,CGRectMake(0.0, 0.0,size.width,  size.height), im.CGImage);
-	UIImage* imageOut =UIGraphicsGetImageFromCurrentImageContext();
-	UIGraphicsEndImageContext();
-	return imageOut;
-}
-
 
 - (void)didReceiveMemoryWarning
 {
@@ -457,105 +432,6 @@ cv::Mat debugSquares( std::vector<std::vector<cv::Point> > squares, cv::Mat imag
     }
     
     return image;
-}
-
-
-UIImage *scaleAndRotateImage(UIImage *image, int maxPixelsAmount)
-{
-	CGImageRef imgRef = image.CGImage;
-	
-	CGFloat width = CGImageGetWidth(imgRef);
-	CGFloat height = CGImageGetHeight(imgRef);
-	
-	// Reduce image width and height by 2^n times to match condition: width*height <= maxPixelAmount,
-	// where n = floor( log4( maxPixelsAmount / (width * height) ) )
-	CGFloat scaleRatio = fmin( 1., pow( 2., floor( 1e-12 + log2( (double)maxPixelsAmount / (width * height) ) / 2. ) ) );
-	
-	CGAffineTransform transform = CGAffineTransformIdentity;
-	CGRect bounds = CGRectMake(0, 0, width, height);
-	bounds.size.width = width * scaleRatio;
-	bounds.size.height = height * scaleRatio;
-	
-	CGSize imageSize = CGSizeMake(CGImageGetWidth(imgRef), CGImageGetHeight(imgRef));
-	CGFloat boundHeight;
-	UIImageOrientation orient = image.imageOrientation;
-	switch(orient) {
-		case UIImageOrientationUp:
-			transform = CGAffineTransformIdentity;
-			break;
-			
-		case UIImageOrientationUpMirrored:
-			transform = CGAffineTransformMakeTranslation(imageSize.width, 0.0);
-			transform = CGAffineTransformScale(transform, -1.0, 1.0);
-			break;
-			
-		case UIImageOrientationDown:
-			transform = CGAffineTransformMakeTranslation(imageSize.width, imageSize.height);
-			transform = CGAffineTransformRotate(transform, M_PI);
-			break;
-			
-		case UIImageOrientationDownMirrored:
-			transform = CGAffineTransformMakeTranslation(0.0, imageSize.height);
-			transform = CGAffineTransformScale(transform, 1.0, -1.0);
-			break;
-			
-		case UIImageOrientationLeftMirrored:
-			boundHeight = bounds.size.height;
-			bounds.size.height = bounds.size.width;
-			bounds.size.width = boundHeight;
-			transform = CGAffineTransformMakeTranslation(imageSize.height, imageSize.width);
-			transform = CGAffineTransformScale(transform, -1.0, 1.0);
-			transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
-			break;
-			
-		case UIImageOrientationLeft:
-			boundHeight = bounds.size.height;
-			bounds.size.height = bounds.size.width;
-			bounds.size.width = boundHeight;
-			transform = CGAffineTransformMakeTranslation(0.0, imageSize.width);
-			transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
-			break;
-			
-		case UIImageOrientationRightMirrored:
-			boundHeight = bounds.size.height;
-			bounds.size.height = bounds.size.width;
-			bounds.size.width = boundHeight;
-			transform = CGAffineTransformMakeScale(-1.0, 1.0);
-			transform = CGAffineTransformRotate(transform, M_PI / 2.0);
-			break;
-			
-		case UIImageOrientationRight:
-			boundHeight = bounds.size.height;
-			bounds.size.height = bounds.size.width;
-			bounds.size.width = boundHeight;
-			transform = CGAffineTransformMakeTranslation(imageSize.height, 0.0);
-			transform = CGAffineTransformRotate(transform, M_PI / 2.0);
-			break;
-			
-		default:
-			[NSException raise:NSInternalInconsistencyException format:@"Invalid image orientation"];
-	}
-	
-	UIGraphicsBeginImageContext(bounds.size);
-	
-	CGContextRef context = UIGraphicsGetCurrentContext();
-	
-	if (orient == UIImageOrientationRight || orient == UIImageOrientationLeft) {
-		CGContextScaleCTM(context, -scaleRatio, scaleRatio);
-		CGContextTranslateCTM(context, -height, 0);
-	}
-	else {
-		CGContextScaleCTM(context, scaleRatio, -scaleRatio);
-		CGContextTranslateCTM(context, 0, -height);
-	}
-	
-	CGContextConcatCTM(context, transform);
-	
-	CGContextDrawImage(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, width, height), imgRef);
-	UIImage *imageCopy = UIGraphicsGetImageFromCurrentImageContext();
-	UIGraphicsEndImageContext();
-	
-	return imageCopy;
 }
 
 
